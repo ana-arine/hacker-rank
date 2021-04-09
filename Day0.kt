4c942fd219ba81af6db0a8d86f4aaa34a6c52b69
Ana Arine <analucia.arine@gmail.com>
2021-04-09 16:13:09 -0300

    Solve Day 0

---
+++ Day0.kt
@@ -0,0 +1,13 @@
+
+//To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World.
+// on a single line, and finally print the value of your variable on a second line.
+
+fun main(args: Array<String>) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+
+    val secondLine: String = readLine()!!
+
+    println("Hello, World.")
+    println(secondLine)
+
+}
